e2200b007cee3e199158fb1287d993b6085852d9
Manoj Tiwari <mj@amritas-mbp.lan>
2023-01-28 01:16:28 -0800

    Few buttons and text box

--- app.py
+++ app.py
@@ -14,5 +14,16 @@ st.set_page_config(page_title="Cover letter", page_icon="🤖", layout="wide")
 
 with st.container():
   st.title("Generate Cover letters")
-  # TODO: update githbub and add linkedin profile
-  st.markdown("This mini-app generates cover letters using OpenAI's GPT-3 based [Davinci model](https://beta.openai.com/docs/models/overview). You can find the code on [GitHub](https://github.com/kinosal/tweet) and the author on [LinkedIn](https://www.linkedin.com/in/manoj-tiwari-17b9213/).")
+  st.markdown("This mini-app generates cover letters using OpenAI's GPT-3 based [Davinci model](https://beta.openai.com/docs/models/overview). You can find the code on [GitHub](https://github.com/gitmj/cover_letter) and the author on [LinkedIn](https://www.linkedin.com/in/manoj-tiwari-17b9213/).")
+
+job = st.text_input(label="Job Description", placeholder="")
+profile = st.text_input(label="LinkedIn Profile", placeholder="")
+
+#col1, col2 = st.columns(2)
+#with col1:
+#    st.session_state.feeling_lucky = not st.button(
+#        label="Generate text",
+#        type="primary",
+#        on_click=generate_text,
+#        args=(topic, mood, style),
+#    )
